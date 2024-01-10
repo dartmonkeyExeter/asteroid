f7a66a903fa803c0936eb8d61d63e80b3b64d651
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2024-01-10 14:43:14 +0000

    im gonna tear my eyeballs from my skull

--- main.py
+++ main.py
@@ -3,26 +3,35 @@ import math
 
 pygame.init()
 win = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
-fps = 30
+fps = 60
 clock = pygame.time.Clock()
 
-class player:
-    def __init__(self, p1x, p2x, p3x, p1y, p2y, p3y):
-        self.p1x = p1x
-        self.p2x = p2x
-        self.p3x = p3x
-        self.p1y = p1y
-        self.p2y = p2y
-        self.p3y = p3y
-        self.yvel = 0
-        self.xvel = 0
-        self.angle = 0
+class Player:
+    def __init__(self, position, angle):
+        self.position = pygame.math.Vector2(position)
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.acceleration = 0.25
+        self.deceleration = 0.05
+        self.max_speed = 5.0
+        self.angle = angle
 
-new_player = player(200, 212.5, 206.25, 200, 200, 230)
+new_player = Player((200, 200), 270)
 
-radius = new_player.p3y - new_player.p1y
+class Bullet:
+    def __init__(self, position, velocity):
+        self.position = pygame.math.Vector2(position)
+        self.velocity = pygame.math.Vector2(velocity)
+        self.min_speed = 5.0
+
+triangle_base = 40
+triangle_size = 20  # Adjust the size of the triangle
+
+bullets = []
 
 running = True
+
+previous_velocity = pygame.math.Vector2(0, 1)
+
 while running:
     clock.tick(fps)
     new_player.angle = new_player.angle % 360
@@ -30,41 +39,57 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
-        
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            nose_position = new_player.position + pygame.math.Vector2(triangle_base / 2, 0).rotate(-new_player.angle)
+
+            new_bullet = Bullet(nose_position, previous_velocity)
+            new_bullet.velocity.y -= new_bullet.min_speed * math.sin(math.radians(new_player.angle))
+            new_bullet.velocity.x += new_bullet.min_speed * math.cos(math.radians(new_player.angle))
+            bullets.append(new_bullet)
+
+
     win.fill((0, 0, 0))
-    point_to_rotate_by = (new_player.p1x + ((new_player.p2x - new_player.p1x) / 2), new_player.p1y)
-    angle = 0
     
     keys = pygame.key.get_pressed()
-
     if keys[pygame.K_w]:
-        new_player.p1y -= 5
-        new_player.p2y -= 5
-        new_player.p3y -= 5
-    
-    if keys[pygame.K_a]:
-        angle += 2.5
+        new_player.velocity.y -= new_player.acceleration * math.sin(math.radians(new_player.angle))
+        new_player.velocity.x += new_player.acceleration * math.cos(math.radians(new_player.angle))
+        
+        # Cap the speed
+        previous_velocity = new_player.velocity
+        new_player.velocity.scale_to_length(min(new_player.velocity.length(), new_player.max_speed))
+    else:
+        # Deceleration when 'W' is not pressed
+        new_player.velocity *= 1 - new_player.deceleration
+    if keys[pygame.K_d]:
         new_player.angle += 2.5
 
-    if keys[pygame.K_d]:
-        angle -= 2.5
+    if keys[pygame.K_a]:
         new_player.angle -= 2.5
-
-    if angle > 0:
-        angle -= 2.5
     
-    new_player.p1x = (new_player.p1x - point_to_rotate_by[0]) * math.cos(math.radians(angle)) - (new_player.p1y - point_to_rotate_by[1]) * math.sin(math.radians(angle)) + point_to_rotate_by[0]
-    new_player.p1y = (new_player.p1x - point_to_rotate_by[0]) * math.sin(math.radians(angle)) + (new_player.p1y - point_to_rotate_by[1]) * math.cos(math.radians(angle)) + point_to_rotate_by[1]
-    new_player.p2x = (new_player.p2x - point_to_rotate_by[0]) * math.cos(math.radians(angle)) - (new_player.p2y - point_to_rotate_by[1]) * math.sin(math.radians(angle)) + point_to_rotate_by[0]
-    new_player.p2y = (new_player.p2x - point_to_rotate_by[0]) * math.sin(math.radians(angle)) + (new_player.p2y - point_to_rotate_by[1]) * math.cos(math.radians(angle)) + point_to_rotate_by[1]
-    new_player.p3x = (new_player.p3x - point_to_rotate_by[0]) * math.cos(math.radians(angle)) - (new_player.p3y - point_to_rotate_by[1]) * math.sin(math.radians(angle)) + point_to_rotate_by[0]
-    new_player.p3y = (new_player.p3x - point_to_rotate_by[0]) * math.sin(math.radians(angle)) + (new_player.p3y - point_to_rotate_by[1]) * math.cos(math.radians(angle)) + point_to_rotate_by[1]
-
-    pygame.draw.polygon(win, (255, 255, 255), ((new_player.p1x, new_player.p1y),
-                                                (new_player.p2x, new_player.p2y), 
-                                                (new_player.p3x, new_player.p3y)), 2)
-
+    new_player.position += new_player.velocity
     
+    rotated_point1 = new_player.position + pygame.math.Vector2(triangle_base / 2, 0).rotate(-new_player.angle)
+    rotated_point2 = new_player.position + pygame.math.Vector2(triangle_size / 2, 0).rotate(-new_player.angle + 120)
+    rotated_point3 = new_player.position + pygame.math.Vector2(triangle_size / 2, 0).rotate(-new_player.angle - 120)
+
+    if new_player.position.x > 500:
+        new_player.position.x = 0
+    elif new_player.position.x < 0:
+        new_player.position.x = 500
+    if new_player.position.y > 500:
+        new_player.position.y = 0
+    elif new_player.position.y < 0:
+        new_player.position.y = 500
+
+    if len(bullets) > 4:
+        bullets.pop(0)
+
+    pygame.draw.polygon(win, (255, 255, 255), (rotated_point1, rotated_point2, rotated_point3), 1)
+    for bullet in bullets:
+        bullet.position += bullet.velocity
+        pygame.draw.circle(win, (255, 255, 255), (int(bullet.position.x), int(bullet.position.y)), 1)
+
     pygame.display.update()
 
+pygame.quit()
